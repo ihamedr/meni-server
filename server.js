500572f6e05f196b409f3d90f98d3ae537c9b509
ihamedr <amirhamedrazavi@gmail.com>
2025-04-28 16:58:21 +0330

    Add files via upload

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.post('/like', async (req, res) => {
 
   try {
     const result = await cloudinary.search
-      .expression(public_id=${memeId})
+      .expression(`public_id=${memeId}`)
       .max_results(1)
       .execute();
 
